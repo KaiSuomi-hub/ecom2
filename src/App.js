0fcf1159803d63384929b6a115e674388ac8a59e
KaiSuomi-hub <suomen.perhe@gmail.com>
2022-02-16 14:46:12 +0200

    trying to fix old rev router to v6. Sort of hacking it. Still missing the history function

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import HomePage from './page/homepage/homapage.component';
+import {
+  Route,
+  Routes,
+  useParams,
+} from 'react-router-dom';
 
-class App extends Component {
-  render() {
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+
+    <h1>Hats!</h1>
+
+    )
+};
+
+
+
+function  App() {
     return (
       <div>
-      <HomePage />
+          <Routes>
+          <Route path="/" element={<HomePage/>} ></Route>
+          <Route path="/hats" element={<HatsPage />} ></Route>
+          <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+            }
+            />
+          </Routes>
+
       </div>
     );
-  }
 }
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import './menu-item.styles.css';
+import {   useLocation,
+	useNavigate,
+	useParams,
+} from 'react-router-dom';
 
-const Menuitem = ({title,imageUrl, size}) => (
-	<div className={`${size} menu-item`}>
+// this is a simulated withRouter
+// v6 react-routed-dom doesn't have it
+function withRouter(Component) {
+	function ComponentWithRouterProp(props) {
+	  let location = useLocation();
+	  let navigate = useNavigate();
+	  let params = useParams();
+	  return (
+		<Component
+		  {...props}
+		  router={{ location, navigate, params }}
+		/>
+	  );
+	}
+
+	return ComponentWithRouterProp;
+}
+
+const Menuitem = ({ title, imageUrl, size, history, linkUrl}) => {
+	const navigate = useNavigate();
+
+	return(
+	<div className={`${size} menu-item`}
+			onClick={() => navigate(`${linkUrl}`)}>
+			{/* okay, so here we use navigate instead of history.push as we use v6 react-router-dom
+			notice the need to use blank arrow function with onClick */}
 		<div className="background-image"
 		style={{
 		backgroundImage:`url(${imageUrl})`
@@ -13,7 +41,8 @@ const Menuitem = ({title,imageUrl, size}) => (
 			<div className="title">{title.toUpperCase()}</div>
 			<span className="subtitle">BUY NOW!</span>
 		</div>
-	</div>
-)
+		</div>
+		)
+}
 
-export default Menuitem;
+export default withRouter(Menuitem);
